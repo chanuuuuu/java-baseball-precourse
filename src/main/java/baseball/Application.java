daa37045d17427fc79c9865fb087dbab95487dd8
chanuuuuu <dn0208@gmail.com>
2022-01-11 12:34:54 +0900

    [Refactor] indent depth 규칙을 위한 게임 로직 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,22 +8,33 @@ public class Application {
         playGame();
     }
 
-    public static void playGame() {
+    private static void playGame() {
         Pitcher pitcher = new Pitcher(BALL_COUNTS);
         while(true) {
             System.out.print("숫자를 입력해 주세요 : ");
             String 사용자입력값 = GameConsole.read(BALL_COUNTS);
-            boolean 라운드종료여부 = pitcher.round(사용자입력값);
-            if(라운드종료여부) {
-                boolean 게임지속여부 = checkGameContinue();
-                if(게임지속여부) {
-                    pitcher.init();
-                } else {
-                    System.out.println("게임 끝");
-                    break;
-                }
+            boolean 게임지속여부 = playRound(pitcher, 사용자입력값);
+            if(게임지속여부) {
+                pitcher.init();
+            } else {
+                System.out.println("게임 끝");
+                break;
             }
         }
+    }
+
+    /**
+     * 사용자 입력값을 통해 라운드를 진행하고 라운드 종료 조건시에 게임지속여부를 판단한다.
+     * @param pitcher       게임을 진행할 난수를 가진 컴퓨터
+     * @param 사용자입력값     검증된 사용자의 입력값
+     * @return  boolean     게임 지속 여부
+     */
+    private static boolean playRound(final Pitcher pitcher, final String 사용자입력값) {
+        boolean 라운드종료여부 = pitcher.round(사용자입력값);
+        if(라운드종료여부) {
+            return  checkGameContinue();
+        }
+        return false;
     }
 
     /**
